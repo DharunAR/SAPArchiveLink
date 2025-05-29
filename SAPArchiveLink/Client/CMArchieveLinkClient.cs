41b96584b833fafc4f30e3bad4fb377ad1eba332
asamal <asamal@opentext.com>
2025-05-29 11:57:10 +0530

    Added archieveclient.cs file

---
+++ SAPArchiveLink/Client/CMArchieveLinkClient.cs
@@ -0,0 +1,15 @@
+﻿namespace SAPArchiveLink
+{
+    //All CM SDK calls will be written here
+    public class CMArchieveLinkClient
+    {
+        public CMArchieveLinkClient()
+        {
+            
+        }
+        public void PutArchiveCertificate()
+        {
+
+        }
+    }
+}
