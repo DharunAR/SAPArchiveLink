a552225e09d6dd2ed0879e7a6f7ddb76fb2311fb
asamal <asamal@opentext.com>
2025-05-30 10:33:32 +0530

    Added Unittest case project

---
+++ SAPArchiveLink.Tests/CMArchieveLinkClientTest.cs
@@ -0,0 +1,16 @@
+namespace SAPArchiveLink.Tests
+{
+    public class CMArchieveLinkClientTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
