f247295aea7f7fde34cbe2a4050f29b392229828
asamal <asamal@opentext.com>
2025-06-06 13:04:50 +0530

    Added more unittest case

---
+++ SAPArchiveLink.Tests/Services/Handlers/PutCertCommandHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using Moq;
+
+namespace SAPArchiveLink.Tests
+{
+    [TestFixture]
+    public class PutCertCommandHandlerTests
+    {
+        private Mock<ILogHelper<PutCertCommandHandler>> _mockLogger;
+        private Mock<ICommandResponseFactory> _mockResponseFactory;
+        private Mock<IBaseServices> _mockBaseServices;
+        private Mock<ICommand> _mockCommand;
+        private Mock<ICommandRequestContext> _mockContext;
+        private PutCertCommandHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockLogger = new Mock<ILogHelper<PutCertCommandHandler>>();
+            _mockResponseFactory = new Mock<ICommandResponseFactory>();
+            _mockBaseServices = new Mock<IBaseServices>();
+            _mockCommand = new Mock<ICommand>();
+            _mockContext = new Mock<ICommandRequestContext>();
+
+            _handler = new PutCertCommandHandler(
+                _mockLogger.Object,
+                _mockResponseFactory.Object,
+                _mockBaseServices.Object
+            );
+        }
+
+        [Test]
+        public async Task HandleAsync_Success_ReturnsCommandResponse()
+        {
+            // Arrange
+            var contRep = "CONTREP";
+            var authId = "AUTHID";
+            var permissions = "PERM";
+            var fakeStream = new MemoryStream();
+            var expectedResponse = Mock.Of<ICommandResponse>();
+
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarContRep)).Returns(contRep);
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarAuthId)).Returns(authId);
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarPermissions)).Returns(permissions);
+            _mockContext.Setup(c => c.GetInputStream()).Returns(fakeStream);
+            _mockBaseServices.Setup(s => s.PutCert(authId, fakeStream, contRep, permissions))
+                .ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _handler.HandleAsync(_mockCommand.Object, _mockContext.Object);
+
+            // Assert
+            Assert.That(result, Is.SameAs(expectedResponse));          
+            _mockLogger.Verify(l => l.LogInformation(It.Is<string>(s => s.Contains("Start processing"))), Times.Once);
+        }
+
+        [Test]
+        public void HandleAsync_WhenBaseServicesThrows_ThrowsALException()
+        {
+            // Arrange
+            var contRep = "CONTREP";
+            var authId = "AUTHID";
+            var permissions = "PERM";
+            var fakeStream = new MemoryStream();
+
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarContRep)).Returns(contRep);
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarAuthId)).Returns(authId);
+            _mockCommand.Setup(c => c.GetValue(ALParameter.VarPermissions)).Returns(permissions);
+            _mockContext.Setup(c => c.GetInputStream()).Returns(fakeStream);
+            _mockBaseServices.Setup(s => s.PutCert(authId, fakeStream, contRep, permissions))
+                .ThrowsAsync(new InvalidOperationException("fail"));
+
+            // Act & Assert
+            var ex = Assert.ThrowsAsync<ALException>(async () =>
+                await _handler.HandleAsync(_mockCommand.Object, _mockContext.Object));
+            Assert.That(ex.InnerException, Is.TypeOf<InvalidOperationException>());
+            Assert.That(ex.InnerException.Message, Is.EqualTo("fail"));
+        }
+    }
+}
