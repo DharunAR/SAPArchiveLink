11b9546a960b8f60e7f88eaf4a2b0864802f9c4b
asamal <asamal@opentext.com>
2025-06-30 09:25:31 +0530

    added excel extractor

---
+++ SAPArchiveLink/Helpers/ExcelTextExtractor.cs
@@ -0,0 +1,27 @@
+﻿using ClosedXML.Excel;
+using System.Text;
+
+namespace SAPArchiveLink
+{
+    public class ExcelTextExtractor : ITextExtractor
+    {
+        public string ExtractText(Stream stream)
+        {
+            var sb = new StringBuilder();
+            using var workbook = new XLWorkbook(stream);
+            foreach (var worksheet in workbook.Worksheets)
+            {
+                sb.AppendLine($"Sheet: {worksheet.Name}");
+                foreach (var row in worksheet.RowsUsed())
+                {
+                    foreach (var cell in row.Cells())
+                    {
+                        sb.Append(cell.Value.ToString()).Append("\t");
+                    }
+                    sb.AppendLine();
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
