fb35650f275d03635680fa58678ae9196d058fe8
asamal <asamal@opentext.com>
2025-07-15 10:48:36 +0530

    Checked-in missed file

---
+++ SAPArchiveLink/Models/ArchiveCounter.cs
@@ -0,0 +1,16 @@
+﻿namespace SAPArchiveLink
+{
+    public class ArchiveCounter
+    {
+        public int CreateCount { get; private set; }
+        public int DeleteCount { get; private set; }
+        public int UpdateCount { get; private set; }
+        public int ViewCount { get; private set; }
+
+        public void IncrementCreate(int value) => CreateCount += value;
+        public void IncrementDelete(int value) => DeleteCount += value;
+        public void IncrementUpdate(int value) => UpdateCount += value;
+        public void IncrementView(int value) => ViewCount += value;
+    }
+
+}
