12a55aeced04153e38e79613d629a8d2de0e73b2
dharunkumara <dharunkumara@opentext.com>
2025-07-25 11:12:58 +0530

    Removed unused handlers

--- SAPArchiveLink/ServiceRegistration.cs
+++ SAPArchiveLink/ServiceRegistration.cs
@@ -15,55 +15,20 @@
             services.AddScoped<ContentServerRequestAuthenticator>();
 
             // Register all ICommandHandler implementations
-            services.AddScoped<ICommandHandler, AdmInfoCommandHandler>();
-            services.AddScoped<ICommandHandler, AdminContRepCommandHandler>();
-            services.AddScoped<ICommandHandler, AnalyzeSecCommandHandler>();
             services.AddScoped<ICommandHandler, AppendCommandHandler>();
             services.AddScoped<ICommandHandler, AttrSearchCommandHandler>();
-            services.AddScoped<ICommandHandler, CacheCommandHandler>();
-            services.AddScoped<ICommandHandler, CreatePlaceholderCommandHandler>();
             services.AddScoped<ICommandHandler, CreatePutCommandHandler>();
             services.AddScoped<ICommandHandler, CreatePostCommandHandler>();
-            services.AddScoped<ICommandHandler, CsInfoCommandHandler>();
-            services.AddScoped<ICommandHandler, CsrvInfoCommandHandler>();
-            services.AddScoped<ICommandHandler, CsrvSettingsCommandHandler>();
-            services.AddScoped<ICommandHandler, DelAttributeCommandHandler>();
             services.AddScoped<ICommandHandler, DeleteCommandHandler>();
             services.AddScoped<ICommandHandler, DocGetCommandHandler>();
-            services.AddScoped<ICommandHandler, DocGetFromCsCommandHandler>();
-            services.AddScoped<ICommandHandler, FlushCommandHandler>();
-            services.AddScoped<ICommandHandler, FreeSearchCommandHandler>();
             services.AddScoped<ICommandHandler, GetCommandHandler>();
-            services.AddScoped<ICommandHandler, GetHeadCommandHandler>();
-            services.AddScoped<ICommandHandler, GetAttributeCommandHandler>();
-            services.AddScoped<ICommandHandler, GetAtsCommandHandler>();
-            services.AddScoped<ICommandHandler, GetCertCommandHandler>();
-            services.AddScoped<ICommandHandler, GetDocHistoryCommandHandler>();
             services.AddScoped<ICommandHandler, InfoCommandHandler>();
-            services.AddScoped<ICommandHandler, LockCommandHandler>();
             services.AddScoped<ICommandHandler, MCreateCommandHandler>();
-            services.AddScoped<ICommandHandler, MigrateCommandHandler>();
             services.AddScoped<ICommandHandler, PutCertCommandHandler>();
-            services.AddScoped<ICommandHandler, ReinitCommandHandler>();
-            services.AddScoped<ICommandHandler, ReserveDocIdCommandHandler>();
             services.AddScoped<ICommandHandler, SearchCommandHandler>();
             services.AddScoped<ICommandHandler, ServerInfoCommandHandler>();
-            services.AddScoped<ICommandHandler, SetAttributeCommandHandler>();
-            services.AddScoped<ICommandHandler, SetDocFlagCommandHandler>();
-            services.AddScoped<ICommandHandler, SetRecordCommandHandler>();
-            services.AddScoped<ICommandHandler, SignUrlCommandHandler>();
-            services.AddScoped<ICommandHandler, UnlockCommandHandler>();
             services.AddScoped<ICommandHandler, UpdateCommandHandler>();
             services.AddScoped<ICommandHandler, UpdatePostCommandHandler>();
-            services.AddScoped<ICommandHandler, ValidUserCommandHandler>();
-            services.AddScoped<ICommandHandler, VerifyAtsCommandHandler>();
-            services.AddScoped<ICommandHandler, VerifySigCommandHandler>();
-            //services.AddScoped<ICommandHandler, AppendNoteCommandHandler>();
-            //services.AddScoped<ICommandHandler, GetNotesCommandHandler>();
-            services.AddScoped<ICommandHandler, StoreAnnotationsCommandHandler>();
-            services.AddScoped<ICommandHandler, GetAnnotationsCommandHandler>();
-            services.AddScoped<ICommandHandler, DistributeContentCommandHandler>();
-            services.AddScoped<ICommandHandler, GetContentCommandHandler>();
 
             services.AddScoped<IDatabaseConnection, DatabaseConnection>();
 
@@ -86,14 +51,14 @@
             TextExtractorFactory.Register("text/plain", new PlainTextExtractor());
             TextExtractorFactory.Register("application/pdf", new PdfTextExtractor());
             TextExtractorFactory.Register("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", new ExcelTextExtractor());
-            TextExtractorFactory.Register("application/vnd.openxmlformats-officedocument.wordprocessingml.document", new DocxTextExtractor());           
+            TextExtractorFactory.Register("application/vnd.openxmlformats-officedocument.wordprocessingml.document", new DocxTextExtractor());
         }
         private static void RegisterContentAppender()
         {
             DocumentAppenderFactory.Register(".txt", new TextDocumentAppender());
             DocumentAppenderFactory.Register(".pdf", new PdfDocumentAppender());
             DocumentAppenderFactory.Register(".docx", new WordDocumentAppender());
-            DocumentAppenderFactory.Register(".xlxs", new ExcelDocumentAppender());           
+            DocumentAppenderFactory.Register(".xlxs", new ExcelDocumentAppender());
         }
 
         /// <summary>
